668405523a71e9e656b4a384aea590d88663df90
YongzhiGao <gaoyongzhi0601@163.com>
2023-09-27 14:14:58 -0400

    Update codes for lab 5

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,53 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private SortedIntList innerList;
+    private int totalAdded;
 
+    public DelegationSortedIntList(){
+        this.innerList = new SortedIntList();
+        this.totalAdded = 0;
+    }
+
+    public boolean add(int num){
+        totalAdded++;
+        return innerList.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();  // Increment the count by the size of the incoming list.
+        return innerList.addAll(list);  // Delegate to the inner list.
+    }
+
+    @Override
+    public int get(int index) {
+        return innerList.get(index);  // Delegate to the inner list.
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return innerList.remove(num);  // Delegate to the inner list.
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return innerList.removeAll(list);  // Delegate to the inner list.
+    }
+
+    @Override
+    public int size() {
+        return innerList.size();  // Delegate to the inner list.
+    }
+
+    /**
+     * Returns the total number of elements that have been added to the list.
+     *
+     * @return total number of elements added
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,5 +1,4 @@
 package edu.cmu.cs.cs214.rec04;
-
 /**
  * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
  * count of the number of attempted element insertions (not to be confused
@@ -10,7 +9,28 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
 
+    private int totalAdded;
+
+    public InheritanceSortedIntList(){
+        super();
+        this.totalAdded = 0;
+    }
+    
+    @Override
+    public boolean add(int num){
+        totalAdded++;
+        return super.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list){
+        return super.addAll(list);
+    }
+
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
